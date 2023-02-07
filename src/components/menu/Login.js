09447652e799e90a62762ef1f88c15862ac2e51e
chinchin <skykim10908@naver.com>
2023-02-07 15:30:21 +0900

    project init

---
+++ src/components/menu/Login.js
@@ -0,0 +1,35 @@
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+
+function Login()
+{
+    return (
+        <Container className={'vh-100'}>
+            <Row className={'vh-100'}>
+                <Col className={'d-flex align-items-center justify-content-center'}>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Login;
