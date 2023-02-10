470dca2537d8ba07f9868502034a30b584ee3840
chinchin <skykim10908@naver.com>
2023-02-10 11:31:05 +0900

    dropmenu ing..

--- src/components/menu/Main.js
+++ src/components/menu/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Axios from 'axios';
 import { SignIn } from 'api/LoginApi';
 import { useQuery, useMutation, useQueryClient } from 'react-query';
-import { MDBInput, MDBCol, MDBRow, MDBCheckbox, MDBBtn } from 'mdb-react-ui-kit';
+import { MDBContainer, MDBRow, MDBCol, MDBDropdown, MDBDropdownItem, MDBDropdownMenu, MDBDropdownToggle  } from 'mdb-react-ui-kit';
 
 function Login()
 {
@@ -19,9 +19,33 @@ function Login()
     });
     
     return (
-        <div >
-            Main
-        </div>
+        <MDBContainer className='vh-100' fluid>
+            <MDBRow style={{ height : '5%'}}>
+                header
+            </MDBRow>
+            <MDBRow className='' style={{ height : '95%'}}>
+                <MDBCol className='col-9' style={{ height : '100%'}}>
+                    <MDBRow style={{ height : '5%'}} >
+                        <MDBDropdown>
+                            <MDBDropdownToggle>2023</MDBDropdownToggle>
+                            <MDBDropdownMenu>
+                                <MDBDropdownItem childTag='button' className='dropdown-item'>Action</MDBDropdownItem>
+                                <MDBDropdownItem childTag='button' className='dropdown-item'>Another action</MDBDropdownItem>
+                                <MDBDropdownItem childTag='button' className='dropdown-item'>Something else here</MDBDropdownItem>
+                            </MDBDropdownMenu>
+                        </MDBDropdown>
+                    </MDBRow>
+                    <MDBRow className='' style={{ height : '95%'}}>
+                        ??                        
+                        
+
+                    </MDBRow>
+                </MDBCol>
+                <MDBCol>
+                    right
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
     )
 }
 
