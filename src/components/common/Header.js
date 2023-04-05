9718baaadb6d86829b1e40ee6fd5a10389a87992
chinchin <skykim10908@naver.com>
2023-04-05 22:13:37 +0900

    change layout

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,15 +1,13 @@
 import React, { useEffect, useState, useContext } from 'react';
 import MyNavbar from 'components/item/common/MyNavbar';
-import { CommaFormatter } from 'module/Common';
 import { useNavigate } from 'react-router-dom'; 
 import { GlobalContext } from 'context/GlobalContext';
-import { MDBNavbar, MDBContainer, MDBIcon, MDBInputGroup, MDBCol } from 'mdb-react-ui-kit';
-import { MDBCard, MDBCardHeader } from 'mdb-react-ui-kit';
+import { MDBNavbar, MDBContainer, MDBIcon, MDBCol } from 'mdb-react-ui-kit';
 
 function Header()
 {
     // Global State
-    const { GLOBAL_TOKEN, GLOBAL_MONEY } = useContext(GlobalContext);
+    const { GLOBAL_TOKEN } = useContext(GlobalContext);
     
     // Naavigate
     const navigate = useNavigate();
@@ -17,7 +15,6 @@ function Header()
     const onLogOutClicked = () =>
     {
         // 추후 token 만료 처리 요청 필요(임시 로그아웃 구현)
-
         sessionStorage.setItem('ref-token', null );
         GLOBAL_TOKEN.setToken( null );
 
@@ -27,41 +24,8 @@ function Header()
     return (
         <>
             <MDBNavbar expand='lg' light bgColor='white' className='h-100'>
-                <MDBContainer fluid className=''>
-                    <MDBCol className='d-flex justify-content-center align-items-center' >
-                        <MDBCard className='w-25'> 
-                            {
-                                (() =>
-                                {
-                                    const PATH_NAME = window.location.pathname.replace('/', '');
-                                    
-                                    if( PATH_NAME !== 'setting')
-                                    {
-                                        return (
-                                            <MDBCardHeader className='p-0'>
-                                                <MDBInputGroup className=''>
-                                                    <span className='input-group-text'>잔액</span>
-                                                    <input className='form-control' placeholder='' type='number' disabled/>
-                                                    <span className='input-group-text'>지출</span>
-                                                    <input className='form-control text-end' type='text' value={ CommaFormatter(GLOBAL_MONEY.money.expense) } disabled/>
-                                                </MDBInputGroup>
-                                            </MDBCardHeader>
-                                            // <div>
-                                            //     <MDBInputGroup className=''>
-                                            //         {/* <span className="h4">생활비</span> */}
-                                            //         <span className='input-group-text'>잔액</span>
-                                            //         <input className='form-control' placeholder='' type='number' disabled/>
-                                            //         <span className='input-group-text'>지출</span>
-                                            //         <input className='form-control text-end' type='text' value={ CommaFormatter(GLOBAL_MONEY.money.expense) } disabled/>
-                                            //     </MDBInputGroup>
-                                            // </div>
-                                        )
-                                    }
-                                })()
-                            }
-                        </MDBCard>
-                    </MDBCol>
-                    <MDBCol className='col-1 d-flex justify-content-end align-items-center'>
+                <MDBContainer fluid className='d-flex justify-content-center align-items-center'>
+                    <MDBCol className='d-flex justify-content-end align-items-center'>
                         <MDBIcon    className='me-3' 
                                     icon='home' 
                                     size='lg' 
--- src/components/item/main/Left.js
+++ src/components/item/main/Left.js
@@ -1,11 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { CommaFormatter } from 'module/Common';
 import { COMMON_TEXT } from 'module/CommonCode';
+import { GlobalContext } from 'context/GlobalContext';
 import MyCalendar from 'components/item/common/MyCalendar';
-import { MDBDropdown, MDBDropdownItem, MDBDropdownMenu, MDBDropdownToggle  } from 'mdb-react-ui-kit';
+
 import { MDBCard, MDBCardHeader, MDBCardBody } from 'mdb-react-ui-kit';
+import { MDBDropdown, MDBDropdownItem, MDBDropdownMenu, MDBDropdownToggle, MDBInputGroup } from 'mdb-react-ui-kit';
 
 function Left()
 {
+    // Global State
+    const { GLOBAL_MONEY } = useContext(GlobalContext);
+    
     const [calendar, setCalendar] = useState(
     {
          year : new Date().getFullYear()
@@ -22,50 +28,58 @@ function Left()
     return (
         <>
             <MDBCard className='w-100 h-100 m-0'> 
-                <MDBCardHeader style={{ display : 'inline-flex'}}>
-                    <MDBDropdown>
-                        <MDBDropdownToggle>{ calendar.year  + COMMON_TEXT.YEAR }</MDBDropdownToggle>
-                        <MDBDropdownMenu>
-                            <MDBDropdownItem childTag='button' className='dropdown-item'>Action</MDBDropdownItem>
-                            <MDBDropdownItem childTag='button' className='dropdown-item'>Another action</MDBDropdownItem>
-                            <MDBDropdownItem childTag='button' className='dropdown-item'>Something else here</MDBDropdownItem>
-                        </MDBDropdownMenu>
-                    </MDBDropdown>
-                    <MDBDropdown style={{marginLeft : '0.5rem'}}>
-                        <MDBDropdownToggle>{ ( calendar.month + 1 ) + COMMON_TEXT.MONTH }</MDBDropdownToggle>
-                        <MDBDropdownMenu onClick=
-                                         {
-                                            e =>
+                <MDBCardHeader className='d-flex align-items-center position-relative'>
+                    <div style={{ display : 'inline-flex'}}>
+                        <MDBDropdown>
+                            <MDBDropdownToggle>{ calendar.year  + COMMON_TEXT.YEAR }</MDBDropdownToggle>
+                            <MDBDropdownMenu>
+                                <MDBDropdownItem childTag='button' className='dropdown-item'>Action</MDBDropdownItem>
+                                <MDBDropdownItem childTag='button' className='dropdown-item'>Another action</MDBDropdownItem>
+                                <MDBDropdownItem childTag='button' className='dropdown-item'>Something else here</MDBDropdownItem>
+                            </MDBDropdownMenu>
+                        </MDBDropdown>
+                        <MDBDropdown style={{marginLeft : '0.5rem'}}>
+                            <MDBDropdownToggle>{ ( calendar.month + 1 ) + COMMON_TEXT.MONTH }</MDBDropdownToggle>
+                            <MDBDropdownMenu onClick=
                                             {
-                                                const curDate = new Date( calendar.year, Number(e.target.value));
-                                                
-                                                setCalendar( prevState => (
+                                                e =>
                                                 {
-                                                     ...prevState
-                                                    ,month : curDate.getMonth()
-                                                    ,time : curDate.getTime()
-                                                }))
+                                                    const curDate = new Date( calendar.year, Number(e.target.value));
+                                                    
+                                                    setCalendar( prevState => (
+                                                    {
+                                                        ...prevState
+                                                        ,month : curDate.getMonth()
+                                                        ,time : curDate.getTime()
+                                                    }))
+                                                }
                                             }
-                                         }
-                        >
-                            {
-                                (() =>
+                            >
                                 {
-                                    const month = [];
-                                    
-                                    for(let i = 0; i < 12; i++)
+                                    (() =>
                                     {
-                                        month.push
-                                        (
-                                            <MDBDropdownItem key={i} childTag='button' className={'dropdown-item' + ( calendar.month === i ? ' active' : '' ) } value={i}>{ i + 1 + COMMON_TEXT.MONTH }</MDBDropdownItem>
-                                        )
-                                    }
+                                        const month = [];
+                                        
+                                        for(let i = 0; i < 12; i++)
+                                        {
+                                            month.push
+                                            (
+                                                <MDBDropdownItem key={i} childTag='button' className={'dropdown-item' + ( calendar.month === i ? ' active' : '' ) } value={i}>{ i + 1 + COMMON_TEXT.MONTH }</MDBDropdownItem>
+                                            )
+                                        }
 
-                                    return month;
-                                })()
-                            }
-                        </MDBDropdownMenu>
-                    </MDBDropdown>
+                                        return month;
+                                    })()
+                                }
+                            </MDBDropdownMenu>
+                        </MDBDropdown>
+                    </div>
+                    <MDBInputGroup className='w-50 me-4 position-absolute end-0'>
+                        <span className='input-group-text'>잔액</span>
+                        <input className='form-control' placeholder='' type='number' disabled/>
+                        <span className='input-group-text'>지출</span>
+                        <input className='form-control text-end' type='text' value={ CommaFormatter(GLOBAL_MONEY.money.expense) } disabled/>
+                    </MDBInputGroup>
                 </MDBCardHeader>
                 <MDBCardBody className='d-flex justify-content-center'>
                     <MyCalendar year={ calendar.year } month={ calendar.month } time={ calendar.time }/>
