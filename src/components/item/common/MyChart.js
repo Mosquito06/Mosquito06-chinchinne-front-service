a5ac0066163b718c45a63bb0529f854806e108cc
chinchin <skykim10908@naver.com>
2023-04-13 16:55:19 +0900

    add chart color

--- src/components/item/common/MyChart.js
+++ src/components/item/common/MyChart.js
@@ -3,7 +3,7 @@ import AccountApi from 'api/AccountApi';
 import { ResponsivePie } from '@nivo/pie'; 
 import { CommaFormatter } from 'module/Common';
 import { GlobalContext } from 'context/GlobalContext';
-import { MDBCard, MDBCardHeader, MDBCardBody, MDBCheckbox } from 'mdb-react-ui-kit';
+import { MDBCard, MDBCardBody } from 'mdb-react-ui-kit';
 import { COMMON_QUERY_KEYS, COMMON_ACCOUNT_STATUS  } from 'module/CommonCode';
 
 function MyChart()
@@ -82,6 +82,7 @@ function MyChart()
         <MDBCard className='h-100 m-0 p-0'>
             <MDBCardBody className='p-3 pe-2'>
                 <ResponsivePie  data={chart.data}
+                                colors={chart.data.map( row => ( row.color ))}
                                 valueFormat={ ( value ) => CommaFormatter(value) }
                                 margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
                                 innerRadius={0.5}
@@ -129,7 +130,7 @@ function MyChart()
                                             ,justify: false
                                             ,translateX: 0
                                             ,translateY: 56
-                                            ,itemsSpacing: 100 / chart.data.length
+                                            ,itemsSpacing: Math.ceil(100 / chart.data.length)
                                             ,itemWidth: 50
                                             ,itemHeight: 18
                                             ,itemTextColor: '#999'
